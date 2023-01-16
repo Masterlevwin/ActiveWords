de97ba6072ce0c20bf767cbdc3437f1e806fcf3c
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-01-16 12:02:33 +0300

    Update GameBase.cs

--- GameBase.cs
+++ GameBase.cs
@@ -11,6 +11,7 @@ public class GameBase : MonoBehaviour
     public Letter prefab;
     public Wall wall;
     public Sprite[] letters;
+    public List<Letter> word;
     
     void Start()
     {
@@ -27,8 +28,12 @@ public class GameBase : MonoBehaviour
 
     public void InitLevel()
     {
+        if (word == null) word = new List<Letter>();
+        
         Letter let = Instantiate(prefab, new Vector2(Random.Range(-Screen.width/2 + wall.transform.localScale.x, Screen.width/2 - wall.transform.localScale.x),
         Random.Range(-Screen.height/2 + wall.transform.localScale.y, Screen.height/2 - wall.transform.localScale.y)), Quaternion.identity, transform.parent);
         let.GetComponent<SpriteRenderer>().sprite = letters[0];
+        
+        word.Add(let);
     }
 }
