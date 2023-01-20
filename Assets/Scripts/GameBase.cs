094176408e47708eae758be65aae25e943be187f
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-01-20 17:56:13 +0300

    Update GameBase.cs

--- Assets/Scripts/GameBase.cs
+++ Assets/Scripts/GameBase.cs
@@ -9,12 +9,14 @@ public class GameBase : MonoBehaviour
     public static GameBase G;
     
     public TextAsset textAsset;
+    private string[] wordsWithText;
+    private Transform wordAnchor;
     public Text wordLevelText;
+    
     public GameObject prefabLetter;
     public Sprite[] letters;
-    public List<Letter> lets;
-    public Transform wordAnchor;
-    public List<Vector2> spawns;
+    private List<Letter> lets;
+    private List<Vector2> spawns;
     
     void Start()
     {
@@ -26,83 +28,94 @@ public class GameBase : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        InitLevel();
-    }
-
-    public void InitLevel()
-    {
-        if (lets == null) lets = new List<Letter>();
-	if (spawns == null) spawns = new List<Vector2>();
-        if (GameObject.Find("Word") == null)
-	{
-	GameObject wordGO = new GameObject("Word");
-	wordAnchor = wordGO.transform;
-	}
 	
-	string[] words = ParseText(textAsset.text);
-	string wordLevel = words[Random.Range(0, words.Length)];
-	wordLevelText.text = wordLevel;
-        char[] chars = wordLevel.ToCharArray();
-	for (int i = 0; i < chars.Length; i++) MakeLetter(chars[i]);
+	wordsFromText = ParseText(textAsset.text);	// Загружаем в массив все слова из текстового ассета
+	if (GameObject.Find("Word") == null)		// Создаем пустой объект в иерархии, чтобы спрятать туда сгенерированные буквы
+	{
+	    GameObject wordGO = new GameObject("Word");
+	    wordAnchor = wordGO.transform;
+	}
+        InitLevel(wordsFromText);
     }
     
-    private string[] ParseText(string st)
+    private string[] ParseText(string txt)
     {
-    	string[] lines = st.Split("\n"[0]);
+    	string[] lines = txt.Split("\n");
 	return lines;
+    }
+    
+    private void InitLevel(string[] words)
+    {
+	string wordLevel = words[Random.Range(0, words.Length)];	// Выбираем слово для уровня из массива
+	wordLevelText.text = wordLevel;					// Отображаем это слово в канвасе - временно для отладки
+        char[] chars = wordLevel.ToCharArray();				// Преобразуем выбранное слово в массив символов (букв)
+	for (int i = 0; i < chars.Length; i++) MakeLetter(chars[i]);	// Рисуем каждую букву
     }
     
     private void MakeLetter(char l)
     {
-        GameObject letGO = Instantiate(prefabLetter);
-	    letGO.transform.SetParent(wordAnchor);
-        letGO.transform.position = SpawnLetter();
-	    spawns.Add(letGO.transform.position);
-        if (l == 'а') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[0];
-	    else if (l == 'б') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[1];
-        else if (l == 'в') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[2];
-        else if (l == 'г') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[3];
-        else if (l == 'д') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[4];
-        else if (l == 'е') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[5];
-        else if (l == 'ё') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[6];
-        else if (l == 'ж') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[7];
-        else if (l == 'з') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[8];
-        else if (l == 'и') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[9];
-        else if (l == 'й') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[10];
-        else if (l == 'к') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[11];
-        else if (l == 'л') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[12];
-        else if (l == 'м') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[13];
-        else if (l == 'н') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[14];
-        else if (l == 'о') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[15];
-        else if (l == 'п') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[16];
-        else if (l == 'р') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[17];
-        else if (l == 'с') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[18];
-        else if (l == 'т') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[19];
-        else if (l == 'у') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[20];
-        else if (l == 'ф') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[21];
-        else if (l == 'х') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[22];
-        else if (l == 'ц') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[23];
-        else if (l == 'ч') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[24];
-        else if (l == 'ш') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[25];
-        else if (l == 'щ') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[26];
-        else if (l == 'ъ') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[27];
-        else if (l == 'ы') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[28];
-        else if (l == 'ь') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[29];
-        else if (l == 'э') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[30];
-        else if (l == 'ю') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[31];
-        else if (l == 'я') letGO.GetComponentInChildren<SpriteRenderer>().sprite = letters[32];
-        Letter let = letGO.GetComponentInChildren<Letter>();
-        lets.Add(let);
+        GameObject letGO = Instantiate(prefabLetter);		// Инициализируем объект буквы
+	letGO.transform.SetParent(wordAnchor);			// Прячем её в иерархии
+	
+	if (spawns == null) spawns = new List<Vector2>();	// Создаем список мест для букв
+	letGO.transform.position = SpawnLetter();		// Определяем место буквы на игровом поле
+	spawns.Add(letGO.transform.position);			// Полученное место добавляем в список занятых
+	
+        letGO.GetComponentInChildren<SpriteRenderer>().sprite = SetLetterSprite(l);	// Устанавливаем спрайт буквы
+	if (lets == null) lets = new List<Letter>();			// Создаем список букв
+        Letter let = letGO.GetComponentInChildren<Letter>();		// Сохраняем ссылку на класс буквы дочернего объекта
+        lets.Add(let);							// Добавляем букву в список для дальнейшей работы со списком
     }
     
     private Vector2 SpawnLetter()
     {
     	Vector2 radiusLet = new Vector2(0.1f, 0.1f);
-    	Vector2 spawnLet = new Vector2(RandomWithoutFloat(-8f, 8f), RandomWithoutFloat(-3f, 3f));
-	    foreach (Vector2 v in spawns) if (v + radiusLet == spawnLet) return SpawnLetter();
-	    return spawnLet;
+    	Vector2 spawnLet = new Vector2(Random.Range(-8f, 8f), Random.Range(-3f, 3f));
+	
+	foreach (Vector2 v in spawns) if (v + radiusLet == spawnLet) return SpawnLetter();
+	return spawnLet;
     }
     
+    private Sprite SetLetterSprite(char l)
+    {
+    	Sprite spLet = null;
+    	if (l == 'а') spLet = letters[0];
+	else if (l == 'б') spLet = letters[1];
+        else if (l == 'в') spLet = letters[2];
+        else if (l == 'г') spLet = letters[3];
+        else if (l == 'д') spLet = letters[4];
+        else if (l == 'е') spLet = letters[5];
+        else if (l == 'ё') spLet = letters[6];
+        else if (l == 'ж') spLet = letters[7];
+        else if (l == 'з') spLet = letters[8];
+        else if (l == 'и') spLet = letters[9];
+        else if (l == 'й') spLet = letters[10];
+        else if (l == 'к') spLet = letters[11];
+        else if (l == 'л') spLet = letters[12];
+        else if (l == 'м') spLet = letters[13];
+        else if (l == 'н') spLet = letters[14];
+        else if (l == 'о') spLet = letters[15];
+        else if (l == 'п') spLet = letters[16];
+        else if (l == 'р') spLet = letters[17];
+        else if (l == 'с') spLet = letters[18];
+        else if (l == 'т') spLet = letters[19];
+        else if (l == 'у') spLet = letters[20];
+        else if (l == 'ф') spLet = letters[21];
+        else if (l == 'х') spLet = letters[22];
+        else if (l == 'ц') spLet = letters[23];
+        else if (l == 'ч') spLet = letters[24];
+        else if (l == 'ш') spLet = letters[25];
+        else if (l == 'щ') spLet = letters[26];
+        else if (l == 'ъ') spLet = letters[27];
+        else if (l == 'ы') spLet = letters[28];
+        else if (l == 'ь') spLet = letters[29];
+        else if (l == 'э') spLet = letters[30];
+        else if (l == 'ю') spLet = letters[31];
+        else if (l == 'я') spLet = letters[32];
+	else spLet = none;
+	return spLet;
+    }
+	
     public static float RandomWithoutFloat(float from, float to, float without = 0f)
     {
         float res = Random.Range(from, to);
