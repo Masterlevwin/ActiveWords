a497b681969d983dcd87cb2596f66b98b528f7c3
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-01-24 10:00:58 +0300

    Update GameBase.cs

--- Assets/Scripts/GameBase.cs
+++ Assets/Scripts/GameBase.cs
@@ -4,128 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+public enum GamePhase
+{
+    init,
+    complete,
+    pause
+}
+
 public class GameBase : MonoBehaviour
 {
     public static GameBase G;
-    
-    public TextAsset textAsset;
-    private string[] wordsFromText;
-    private Transform wordAnchor;
-    public Text wordLevelText;
-    public LayerMask m_LayerMask;
-    public GameObject prefabLetter;
-    public Sprite[] letters;
-    private List<Letter> lets;
-    private BoxCollider2D table;
-    private Vector2 pos;
+    public GamePhase phase = GamePhase.init;
     
     void Start()
     {
-        if (G == null)
-        {
-            G = this;
-        }
-        else if (G == this)
-        {
-            Destroy(gameObject);
-        }
-	
-	    wordsFromText = ParseText(textAsset.text);	// Загружаем в массив все слова из текстового ассета
-	    if (GameObject.Find("Word") == null)		// Создаем пустой объект в иерархии, чтобы спрятать туда сгенерированные буквы
-	    {
-	        GameObject wordGO = new GameObject("Word");
-	        wordAnchor = wordGO.transform;
-	    }
-	    table = GameObject.Find("BG").GetComponent<BoxCollider2D>();
-	    pos = table.transform.position;
-        InitLevel(wordsFromText);
+        if (G == null) G = this;
+        else if (G == this) Destroy(gameObject);
     }
-    
-    private string[] ParseText(string txt)
-    {
-    	string[] lines = txt.Split("\n");
-	    return lines;
-    }
-    
-    private void InitLevel(string[] words)
-    {
-	    string wordLevel = words[Random.Range(0, words.Length)];	    // Выбираем слово для уровня из массива
-	    wordLevelText.text = wordLevel;					                // Отображаем это слово в канвасе - временно для отладки
-        char[] chars = wordLevel.ToCharArray();				            // Преобразуем выбранное слово в массив символов (букв)
-	    for (int i = 0; i < chars.Length; i++) MakeLetter(chars[i]);	// Рисуем каждую букву
-    }
-    
-    private void MakeLetter(char l)
-    {
-        GameObject letGO = Instantiate(prefabLetter);		            // Инициализируем объект буквы
-	letGO.transform.SetParent(wordAnchor);			                // Прячем её в иерархии
-	    
-        letGO.transform.position = Spawn();                             // Определяем позицию буквы на сцене
-        
-        letGO.GetComponentInChildren<SpriteRenderer>().sprite = SetLetterSprite(l);     // Устанавливаем спрайт буквы
-	if (lets == null) lets = new List<Letter>();			        // Создаем список букв
-        Letter let = letGO.GetComponentInChildren<Letter>();		    // Сохраняем ссылку на класс буквы дочернего объекта
-        lets.Add(let);							                        // Добавляем букву в список для дальнейшей работы со списком
-    }
-    
-    private Vector2 Spawn()
-    {
-    	float x, y;
-	x = Random.Range(pos.x - Random.Range(0, table.bounds.extents.x), pos.x + Random.Range(0, table.bounds.extents.x));
-	y = Random.Range(pos.y - Random.Range(0, table.bounds.extents.y), pos.y + Random.Range(0, table.bounds.extents.y));
-	Vector2 spawnLet = new Vector2(x, y);
-	
-	bool check = Point(spawnLet);
-	if (check) return spawnLet;
-	else return Spawn();
-	bool Point(Vector2 spawn)
-	{
-            Vector2 size = new Vector2(2f, 2f);
-	    Collider2D[] cols = Physics2D.OverlapBoxAll(spawn, size, 0f, m_LayerMask);
-            if (cols.Length > 0) return false;
-	    else return true;
-	}
-    }
-    
-    private Sprite SetLetterSprite(char l)
-    {
-    	Sprite spLet = null;
-    	if (l == 'а') spLet = letters[0];
-        else if (l == 'б') spLet = letters[1];
-        else if (l == 'в') spLet = letters[2];
-        else if (l == 'г') spLet = letters[3];
-        else if (l == 'д') spLet = letters[4];
-        else if (l == 'е') spLet = letters[5];
-        else if (l == 'ё') spLet = letters[6];
-        else if (l == 'ж') spLet = letters[7];
-        else if (l == 'з') spLet = letters[8];
-        else if (l == 'и') spLet = letters[9];
-        else if (l == 'й') spLet = letters[10];
-        else if (l == 'к') spLet = letters[11];
-        else if (l == 'л') spLet = letters[12];
-        else if (l == 'м') spLet = letters[13];
-        else if (l == 'н') spLet = letters[14];
-        else if (l == 'о') spLet = letters[15];
-        else if (l == 'п') spLet = letters[16];
-        else if (l == 'р') spLet = letters[17];
-        else if (l == 'с') spLet = letters[18];
-        else if (l == 'т') spLet = letters[19];
-        else if (l == 'у') spLet = letters[20];
-        else if (l == 'ф') spLet = letters[21];
-        else if (l == 'х') spLet = letters[22];
-        else if (l == 'ц') spLet = letters[23];
-        else if (l == 'ч') spLet = letters[24];
-        else if (l == 'ш') spLet = letters[25];
-        else if (l == 'щ') spLet = letters[26];
-        else if (l == 'ъ') spLet = letters[27];
-        else if (l == 'ы') spLet = letters[28];
-        else if (l == 'ь') spLet = letters[29];
-        else if (l == 'э') spLet = letters[30];
-        else if (l == 'ю') spLet = letters[31];
-        else if (l == 'я') spLet = letters[32];
-        return spLet;
-    }
-    
+
     public static Coroutine Invoke(this MonoBehaviour monoBehaviour, Action action, float time)
     {
         return monoBehaviour.StartCoroutine(InvokeAct(action, time));
