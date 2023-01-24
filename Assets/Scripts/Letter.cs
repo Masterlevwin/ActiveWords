261821e14c09344563f6143a84069c657bac9fce
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-01-24 15:07:18 +0300

    Update Letter.cs

--- Assets/Scripts/Letter.cs
+++ Assets/Scripts/Letter.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Letter : MonoBehaviour, IPointerClickHandler
+public class Letter : MonoBehaviour
 {
     public delegate void DelegateTake(Letter l);
     public DelegateTake takeNotify;
     
-    public void OnPointerClick(PointerEventData eventData)
+    public Vector2 posLet { get; private set };
+    
+    private void OnEnable()
     {
-
+        SetLetterPos(transform.position);
+    }
+    
+    public void SetLetterPos(Vector2 pos)
+    {
+        posLet = pos;
     }
     
     void OnTriggerEnter2D(Collider2D other)
