a6a7296b4543815301c9e8e4e24dee31534bcdd1
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-02-02 11:22:08 +0300

    Update GameBase.cs

--- Assets/Scripts/GameBase.cs
+++ Assets/Scripts/GameBase.cs
@@ -19,17 +19,14 @@ public class GameBase : MonoBehaviour
 {
     public static GameBase G;
     public GamePhase phase = GamePhase.init;
+    
     public static int level = 0;
     public TMP_Text levelText;
     
     public AIPath player;
     public AIPath enemy;
-    //public GameObject block;
+    
     public Init init;
-    public GameObject cellPrefab;
-    public Sprite[] cells;
-    private Transform cellAnchor;
-    private List<Vector2> letPositions;
     private Dictionary<Vector2, Letter> letDict; 
 
     void Start()
@@ -38,39 +35,15 @@ public class GameBase : MonoBehaviour
         else if (G == this) Destroy(gameObject);
         
         init = GetComponent<Init>();
-
-        if (GameObject.Find("Cells") == null)
-        {
-            GameObject cellGO = new GameObject("Cells");
-            cellAnchor = cellGO.transform;
-        }
-        //Instantiate(block, Vector2.zero, Quaternion.identity, transform.parent);
     }
 
     public void StartGame()
     {
         levelText.text = $"{level}";
-        if (letPositions == null) letPositions = new List<Vector2>();
-        else letPositions.Clear();
         if (letDict == null) letDict = new Dictionary<Vector2, Letter>();
         else letDict.Clear();
         if (!player.gameObject.activeSelf) player.gameObject.SetActive(true);
-        player.GetComponent<Player>().hitPlayer = 3;
-        
-        GameObject c;
-        Vector2 cell = Vector2.zero;
-        for (int i = 0; i < init.lets.Count; i++) 
-        {
-            float v = init.lets.Count/2;
-            cell = new Vector2(.5f - v + i, -4.5f);
-            c = Instantiate(cellPrefab, cell, Quaternion.identity, cellAnchor);
-            c.GetComponent<SpriteRenderer>().sprite = cells[0];
-            letPositions.Add(cell);
-        }
-        c = Instantiate(cellPrefab, new Vector2(letPositions[0].x - 2f, letPositions[0].y), Quaternion.identity, cellAnchor);
-        c.GetComponent<SpriteRenderer>().sprite = cells[1];
-        c = Instantiate(cellPrefab, new Vector2(letPositions[letPositions.Count - 1].x + 2f, letPositions[letPositions.Count - 1].y), Quaternion.identity, cellAnchor);
-        c.GetComponent<SpriteRenderer>().sprite = cells[2];
+        player.GetComponent<Player>().hitPlayer = 3; 
         phase = GamePhase.game;
     }
     
@@ -79,8 +52,8 @@ public class GameBase : MonoBehaviour
         phase = GamePhase.pause;
         int numValues = 0;
         
-        for (int i = 0; i < letPositions.Count; i++)
-            if (letDict.TryGetValue(letPositions[i], out Letter l) && l.charLet == init.lets[i].charLet)
+        for (int i = 0; i < init.letPositions.Count; i++)
+            if (letDict.TryGetValue(init.letPositions[i], out Letter l) && l.charLet == init.lets[i].charLet)
                 numValues++;
         if (numValues == init.lets.Count) Win();
         else Lose();
@@ -88,14 +61,12 @@ public class GameBase : MonoBehaviour
 
     private void Win()
     {
-        foreach (Transform child in cellAnchor) Destroy(child.gameObject);
         level++;
         levelText.text = $"{level}";
     }    
     
     private void Lose()
     {
-        foreach (Transform child in cellAnchor) Destroy(child.gameObject);
         level--;
         levelText.text = $"{level}";
     }
@@ -105,7 +76,6 @@ public class GameBase : MonoBehaviour
         if (letDict.ContainsValue(l))
         {
             player.SetPath(null);
-            //enemy.SetPath(null);
             letDict.Remove(l.transform.position);
             StartCoroutine(Move(l, l.posLet, true));
         }
@@ -113,12 +83,12 @@ public class GameBase : MonoBehaviour
     
     public void AddToWord(Letter l)
     {
-        for (int i = 0; i < letPositions.Count; i++)
+        for (int i = 0; i < init.letPositions.Count; i++)
         { 
             if (!letDict.ContainsValue(l) && !letDict.ContainsKey(letPositions[i]))
             {
-                letDict.Add(letPositions[i], l);
-                StartCoroutine(Move(l, letPositions[i], false));
+                letDict.Add(init.letPositions[i], l);
+                StartCoroutine(Move(l, init.letPositions[i], false));
                 break;
             }
         }
