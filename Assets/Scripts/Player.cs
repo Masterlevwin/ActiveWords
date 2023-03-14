d40a2db9c4547727d4a3bb79f691ff783a56a8a9
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-03-14 09:58:16 +0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
         hitPlayer = maxHit;
     }
 
-    public void SetHit(float hit)
+    public virtual void SetHit(float hit)
     {
         hitPlayer = hit;
         txtHp.text = $"{hitPlayer}";
