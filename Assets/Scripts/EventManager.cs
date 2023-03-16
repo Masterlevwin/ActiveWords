2e95e99975b7f0ffd55243358622966454979b56
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-03-16 16:57:36 +0300

    Update EventManager.cs

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-  public static event Action LeaveCreated;
+  public static event Action LeaveCreated, CoinCreated;
   
   public static void OnLeaveCreated() { LeaveCreated?.Invoke(); }
+  public static void OnCoinCreated() { CoinCreated?.Invoke(); }
 }
