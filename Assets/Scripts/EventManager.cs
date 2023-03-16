032641d02955cd49c5ab5b32d161aa9c33caffba
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-03-16 09:29:51 +0300

    Create EventManager.cs

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+  public static event Action LeaveCreated;
+  
+  public static void OnLeaveCreated() { LeaveCreated?.Invoke(); }
+}
