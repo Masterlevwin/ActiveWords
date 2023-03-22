9add9c7814930a9057faaf26a738468358ab29e7
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-03-22 10:02:19 +0300

    Update and rename Timer.cs to TimerEnemyRebirth.cs

--- Assets/Scripts/TimerEnemyRebirth.cs
+++ Assets/Scripts/TimerEnemyRebirth.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class Timer : MonoBehaviour
+public class TimerEnemyRebirth : MonoBehaviour
 {
     [SerializeField] private float time;
     [SerializeField] private TMP_Text timerText;
