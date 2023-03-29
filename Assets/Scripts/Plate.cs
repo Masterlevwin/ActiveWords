7d2783fdd6bb58da22189b8ad7e339501b434f9e
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-03-29 17:14:03 +0300

    Update Plate.cs

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Plate : MonoBehaviour
 {
   private bool isMove = false;
+  private Transform facePlate;
+  
+  void Start()
+  {
+    facePlate = gameObject.transform.GetChild( 1 );
+  }
   
   void OnTriggerEnter2D( Collider2D collision )
   {
@@ -11,10 +17,17 @@ public class Plate : MonoBehaviour
     }
   }
   
+  void OnTriggerExit2D( Collider2D collision )
+  {
+    if( collision.gameObject.tag == "Player" ) {
+      isMove = false;
+    }
+  }
+  
   void Update()
   {
     if( isMove ) {
-      transform.position = Vector2.MoveTowards( transform.position, transform.position + Vector2.right, time.deltaTime );
+      facePlate.transform.position = Vector2.MoveTowards( facePlate.transform.position, facePlate.transform.position + Vector2.right, Time.deltaTime );
     }
   }
 }
