67f9cba4e29307290fd0688eb9fa0a9e9eb5e036
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-04-04 17:56:07 +0300

    Update BoardManager.cs

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -30,72 +30,52 @@ public class BoardManager : MonoBehaviour
     }
   }
   
-void BoardSetup() {
-// Initialize our board
-boardHolder = new GameObject("Board").transform; 
-for (int x = -1; x < columns + 1; x++) { 
- for (int y = -1; y < rows + 1; y++) { 
- // choose a random floor tile and prepare to instantiate it
- GameObject o = gameTiles[Random.Range(0, gameTiles.Length)];
- // If at edges, choose from outer wall tiles instead
- if (x == -1 || x == columns || y == -1 || y == rows) { 
- o = wallTiles[Random.Range(0, wallTiles.Length)];
- } 
- // Instantiate the chosen tile, at current grid position
- GameObject instance = Instantiate(
- o,
- new Vector3(x, y, 0f),
- Quaternion.identity) as GameObject 
- // Set parent of our new instance object to boardHolder
- instance.transform.SetParent(boardHolder);
- }
-}
-}
+  void BoardSetup()
+  {
+    boardHolder = new GameObject("Board").transform; // Initialize our board
+    for (int x = -1; x < columns + 1; x++)
+    { 
+      for (int y = -1; y < rows + 1; y++)
+      { 
+        GameObject o = gameTiles[Random.Range(0, gameTiles.Length)];  // choose a random floor tile and prepare to instantiate it
+        if (x == -1 || x == columns || y == -1 || y == rows)          // If at edges, choose from outer wall tiles instead
+        { 
+          o = wallTiles[Random.Range(0, wallTiles.Length)];
+        }
+        GameObject instance = Instantiate( o, new Vector3(x, y, 0f), Quaternion.identity ) as GameObject; // Instantiate the chosen tile, at current grid position
+        instance.transform.SetParent(boardHolder);  // Set parent of our new instance object to boardHolder   
+      }
+    }
+  }
+ 
+  private Vector3 RandomPosition()
+  {
+    int randomIndex = Random.Range(0, gridPositions.Count); // random index between 0 and total count of items in gridPositions
+    Vector3 randomPosition = gridPositions[randomIndex];    // random position selected from our gridPosition using randomIndex
+    gridPositions.RemoveAt(randomIndex);                    // remove the entry from gridPosition so it can't be re-used
+    return randomPosition;
+  }
 
-Vector3 RandomPosition(){ 
-// random index between 0 and total count of items in gridPositions
-int randomIndex = Random.Range(0, gridPositions.Count); 
-// random position selected from our gridPosition using randomIndex
-Vector3 randomPosition = gridPositions[randomIndex] 
-// remove the entry from gridPosition so it can't be re-used
-gridPositions.RemoveAt(randomIndex); 
-return randomPosition;
-}
+  void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max)
+  {
+    int objectCount = Random.Range(min, max+1);   // random amount to instantiate from given range 
+    for (int i = 0; i < objectCount; i++)         // Place objects at random locations until object count limit
+    { 
+      Vector3 randomPosition = RandomPosition();  // use our helper function to get random position
+      GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)]; // Choose a random tile (pick-up, enemy, etc.)
+      Instantiate(tileChoice, randomPosition, Quaternion.identity);  // Instantiate the chosen tile at the chosen location
+    }
+  }
 
-void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max){
-// random amount to instantiate from given range 
-int objectCount = Random.Range(min, max+1); 
-// Place objects at random locations until object count limit
-for (int i = 0; i < objectCount; i++ { 
- // use our helper function to get random position
- Vector3 randomPosition = RandomPosition(); 
- // Choose a random tile (pick-up, enemy, etc.) 
- GameObject tileChoice = 
- tileArray[Random.Range(0, tileArray.Length)]; 
- // Instantiate the chosen tile at the chosen location
- Instantiate(tileChoice, randomPosition, Quaternion.identity);
-}
-}
-
-public void SetupScene(int level){
-// Call our function to create our outer walls and floor.
-BoardSetup();
-// Re-create our list of valid gridPositions
-InitialiseList();
-// Fill a random number of pick-up tiles 
-LayoutObjectAtRandom(pickupTiles, 1, 3)
-// Determine number of enemies based on current "level"
-// Fill a random number of creatures 
-int enemies = (int)Mathf.Log(level, 2f);
-LayoutObjectAtRandom(enemyTiles, enemies, enemies); 
-// Put in our exit in top right 
-Instantiate(
- exit,
- new Vector3 (columns -1, rows -1, 0f),
- Quaternion.identity
-);
-}
-
-
-
+  public void SetupScene(int level)
+  {
+    BoardSetup();       // Call our function to create our outer walls and floor
+    InitialiseList();   // Re-create our list of valid gridPositions
+    LayoutObjectAtRandom( pickupTiles, 1, 3 ); // Fill a random number of pick-up tiles
+                        // Determine number of enemies based on current "level"
+                        // Fill a random number of creatures 
+    int enemies = (int)Mathf.Log(level, 2f);
+    LayoutObjectAtRandom(enemyTiles, enemies, enemies); 
+    Instantiate( exit, new Vector3 (columns -1, rows -1, 0f), Quaternion.identity );  // Put in our exit in top right
+  }
 }
