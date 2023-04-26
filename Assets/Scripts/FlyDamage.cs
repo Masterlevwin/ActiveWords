a0aa03f9e30f3a93053e6558b6f015f8d8acaac8
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-04-26 10:01:11 +0300

    Update FlyDamage.cs

--- Assets/Scripts/FlyDamage.cs
+++ Assets/Scripts/FlyDamage.cs
@@ -4,17 +4,43 @@ using TMPro;
 public class FlyDamage : MonoBehaviour
 {
     private TMP_Text damageText;
-
+    private float startTime;
+    
     private void Start()
     {
         damageText = GetComponent<TMP_Text>();
     }
-    public void SetDamage( float _damage ) 
+    
+    public void SetDamage( float damage, GameObject go )
     {
-        if (damageText != null)
-        {
-            damageText.text = $" {_damage} ";
-        }
-        else Debug.Log( _damage );
+        if (damageText != null) damageText.text = $"{damage}";
+        else Debug.Log( damage );
+        
+        startTime = Time.time;
+        Vector3 endPosition = go.transform.position + Vector3.up;
+
+        StartCoroutine( Fly( endPosition ) );
+        
+        //end = go.transform.position + Vector3.up;
+        //isFly = true;
     }
+    
+    private IEnumerator Fly( Vector3 endPos, float speed = 1f )
+    {
+        float step = (Time.time - startTime) / speed; 
+        while( Vector2.Distance( transform.position, endPos ) > float.Epsilon )
+        {
+            transform.position = Vector2.Slerp( transform.position, endPos, step );
+            yield return null;
+        }
+        Destroy( gameObject );
+    }
+    
+    /*private bool isFly;
+    private Vector3 end;
+    private void Update()
+    {
+        while( isFly && Vector2.Distance( transform.position, end ) > float.Epsilon )
+            transform.position = Vector2.Slerp( transform.position, end, Time.time - startTime );
+    }*/
 }
