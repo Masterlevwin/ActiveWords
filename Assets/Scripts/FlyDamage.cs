b5ae062dfd45190d5bdc7cd943e717143517ad3b
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-04-26 15:37:15 +0300

    Update FlyDamage.cs

--- Assets/Scripts/FlyDamage.cs
+++ Assets/Scripts/FlyDamage.cs
@@ -6,9 +6,33 @@ public class FlyDamage : MonoBehaviour
     private TMP_Text damageText;
     private float startTime;
     
+    private string damageView;
+    private Vector3 start;
+    
+    public FlyDamage( string damage )
+    {
+        damageView = damage;
+    }
+    
     private void Start()
     {
         damageText = GetComponent<TMP_Text>();
+        
+        //damageText.text = damageView;
+        //start = transform.position;
+        //transform.position += Vector3.down;
+        //StartCoroutine( DamageView() );
+    }
+    
+    private IEnumerator DamageView()
+    {
+        while( transform.positiion.y < start.y - .05f )
+        {
+            transform.position = Vector3.Slerp( transform.position, start, .1f );
+            yield return null;
+        }
+        transform.position = start;
+        Destroy( gameObject );
     }
     
     public void SetDamage( float damage, GameObject go )
