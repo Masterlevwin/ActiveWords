44a9b78c97002d50b1512a34b200f871404649e0
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2023-05-03 09:58:50 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -29,7 +29,7 @@ public class Enemy: MonoBehaviour
         txtAtt = GetComponentsInChildren<TMP_Text>()[1];
         startPos = transform.position;
         ResetProperties();
-        actions = new Action<float>[] { SetHit, SetAttack, SetRebirth, SetSpeed } ;
+        actions = new Action<float>[] { SetAttack, SetRebirth, SetSpeed } ;
     }
     
     public void ResetProperties()
@@ -38,41 +38,50 @@ public class Enemy: MonoBehaviour
         health = 0;
         attack = 0;
         rebirth = 0;
+        GameBase.G.enemy.maxSpeed = 2;
         max_health = start_health;
         SetHit( start_health );
         SetAttack( start_attack );
         SetRebirth( -start_rebirth );
     }
 
+    private void Died()
+    {
+        DiedEnemy?.Invoke( gameObject, 10, rebirth );
+        gameObject.SetActive(false);
+        SetHit( ++max_health );
+        max_health = health;
+        actions[ UnityEngine.Random.Range( 0, actions.Length ) ]( 1f );
+        Waiter.Wait( rebirth, () =>
+        {
+            if( GameBase.G.phase == GamePhase.game )
+            {
+                gameObject.SetActive(true);
+                GameBase.G.enemy.SearchPath();
+                SoundManager.PlaySound( "RebirthEnemy" );
+            }
+        });
+    }
+    
+    public void Damage( float _damage )
+    {
+        health -= _damage;
+        SoundManager.PlaySound("BirdPunch");
+        GameBase.G.FlyDamage( gameObject, GameBase.G.pl.attack_damage );
+        SetHit( health );
+    }
+    
     public void SetHit( float _hp )
     {
-        health += _hp;
+        health = _hp;
         txtHp.text = $"{health}";
         hpImg.fillAmount = health / max_health;
         if( health <= 0 ) Died();
     }
     
-    private void Died()
-    {
-        DiedEnemy?.Invoke( gameObject, 10, rebirth );
-        gameObject.SetActive(false);
-        SetHit( max_health );
-        Waiter.Wait( rebirth, () =>
-        {
-            if( GameBase.G.phase == GamePhase.game ) {
-                actions[ UnityEngine.Random.Range( 0, actions.Length ) ]( 2f );
-                max_health = health;
-                gameObject.SetActive(true);
-                GameBase.G.enemy.SearchPath();
-                SoundManager.PlaySound("RebirthEnemy");
-            }
-        });
-    }
-    
     public void SetAttack( float _attack )
     {
         attack += _attack;
-        if( attack <= 0 ) attack = 1f;
         txtAtt.text = $"{attack}";
     }
     
@@ -84,8 +93,7 @@ public class Enemy: MonoBehaviour
     
     public void SetSpeed( float _speed )
     {
-        GameBase.G.enemy.maxSpeed += _speed - 1f;
-        if( GameBase.G.enemy.maxSpeed <= 0 ) GameBase.G.enemy.maxSpeed = 1f;
+        GameBase.G.enemy.maxSpeed += _speed;
         transform.localScale *= 1.1f;
     }
 
@@ -98,18 +106,16 @@ public class Enemy: MonoBehaviour
     
     void OnTriggerEnter2D( Collider2D collision )
     {
-        if (collision.gameObject.tag == "Player" )
+        if( collision.gameObject.tag == "Player" )
         {
             GameBase.G.pl.Damage( attack );
             SetPos( startPos );
         }
 
         if ( collision.gameObject.tag == "Leave" ) {
-            SetHit( -GameBase.G.pl.attack_damage );
-            GameBase.G.FlyDamage( gameObject, GameBase.G.pl.attack_damage );
+            Damage( GameBase.G.pl.attack_damage );
             collision.gameObject.SetActive( false );
             GameBase.G._leaveActive = false;
-            SoundManager.PlaySound("BirdPunch");
         }
     }
 }
